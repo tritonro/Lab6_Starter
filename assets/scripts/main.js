7eaa4536fb94a03eb385e60ce777d2e4c05153d3
tritonro <106782621+tritonro@users.noreply.github.com>
2025-05-15 23:58:41 -0700

    Completed steps A18-11

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+return JSON.parse(localStorage.getItem('recipes'));
 }
 
 /**
@@ -35,10 +36,20 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	let main = document.getElementsByTagName("main")[0];
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	let newRecipe = document.createElement("recipe-card");
+	recipes.forEach(function (element){
+
+
+		newRecipe.data = element;
+		main.innerHTML(newRecipe);
+		newRecipe = document.createElement("recipe-card");
+		}
+	)
 }
 
 /**
